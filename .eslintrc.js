2ecf582df50a67885cdf2af5f97810c208b15937
kadirozyurt <kadir.ozyurt@gonder.com.tr>
2021-11-24 11:23:17 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: "module"
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+      env: {
+        jest: true
+      }
+    }
+  ]
+}
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <span class="font-weight-bold">Data transferred between children and parents (Emit, Props)</span>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="https://github.com/mustafacagri/vue-data-children-parent" target="_blank" text>
+        <span class="mr-2">Go to Github Repo</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <Home />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+
+  components: {
+    Home
+  },
+
+  data: () => ({
+    //
+  })
+}
+</script>
